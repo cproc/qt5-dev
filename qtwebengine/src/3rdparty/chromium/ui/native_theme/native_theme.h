7100cf133a9b6f9d0b583855f1300954186280ba
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-03-06 17:12:48 +0100

    FreeBSD qtwebengine patches from ports collection

--- qtwebengine/src/3rdparty/chromium/ui/native_theme/native_theme.h
+++ qtwebengine/src/3rdparty/chromium/ui/native_theme/native_theme.h
@@ -48,7 +48,7 @@ class NATIVE_THEME_EXPORT NativeTheme {
   // The part to be painted / sized.
   enum Part {
     kCheckbox,
-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
+#if (defined(OS_LINUX) || defined(OS_BSD)) && !defined(OS_CHROMEOS)
     kFrameTopArea,
 #endif
     kInnerSpinButton,
