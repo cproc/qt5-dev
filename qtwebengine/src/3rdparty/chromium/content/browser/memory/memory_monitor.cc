b5d3f127ae4d2fe7e8fcca59eed671d965a74be0
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-03-06 17:12:48 +0100

    FreeBSD qtwebengine patches from ports collection

--- qtwebengine/src/3rdparty/chromium/content/browser/memory/memory_monitor.cc
+++ qtwebengine/src/3rdparty/chromium/content/browser/memory/memory_monitor.cc
@@ -22,7 +22,7 @@ void MemoryMonitorDelegate::GetSystemMemoryInfo(
   base::GetSystemMemoryInfo(mem_info);
 }
 
-#if defined(OS_MACOSX)
+#if defined(OS_MACOSX) || defined(OS_BSD)
 // TODO(bashi,bcwhite): Remove when memory monitor for mac is available.
 std::unique_ptr<MemoryMonitor> CreateMemoryMonitor() {
   NOTREACHED();
