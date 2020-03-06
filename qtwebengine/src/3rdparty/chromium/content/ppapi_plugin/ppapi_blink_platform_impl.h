e88fe3c78e17780020cf169fca8a770702a7e6b0
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-03-06 17:12:48 +0100

    FreeBSD qtwebengine patches from ports collection

--- qtwebengine/src/3rdparty/chromium/content/ppapi_plugin/ppapi_blink_platform_impl.h
+++ qtwebengine/src/3rdparty/chromium/content/ppapi_plugin/ppapi_blink_platform_impl.h
@@ -13,7 +13,7 @@
 #include "build/build_config.h"
 #include "content/child/blink_platform_impl.h"
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_BSD)
 #include "components/services/font/public/cpp/font_loader.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
 #endif
@@ -42,11 +42,11 @@ class PpapiBlinkPlatformImpl : public BlinkPlatformImpl {
                          bool sync_dir) override;
 
  private:
-#if defined(OS_LINUX) || defined(OS_MACOSX)
+#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_BSD)
   std::unique_ptr<blink::WebSandboxSupport> sandbox_support_;
 #endif
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_BSD)
   sk_sp<font_service::FontLoader> font_loader_;
 #endif
 
--- qtwebengine/src/3rdparty/chromium/third_party/blink/renderer/platform/image-decoders/webp/webp_image_decoder.cc
+++ qtwebengine/src/3rdparty/chromium/third_party/blink/renderer/platform/image-decoders/webp/webp_image_decoder.cc
@@ -32,18 +32,20 @@
 #include "third_party/blink/renderer/platform/histogram.h"
 #include "third_party/skia/include/core/SkData.h"
 
-#if defined(ARCH_CPU_BIG_ENDIAN)
-#error Blink assumes a little-endian target.
-#endif
-
-#if SK_B32_SHIFT  // Output little-endian RGBA pixels (Android).
+#if SK_PMCOLOR_BYTE_ORDER(R, G, B, A)
 inline WEBP_CSP_MODE outputMode(bool hasAlpha) {
   return hasAlpha ? MODE_rgbA : MODE_RGBA;
 }
-#else  // Output little-endian BGRA pixels.
+#elif SK_PMCOLOR_BYTE_ORDER(B, G, R, A)
 inline WEBP_CSP_MODE outputMode(bool hasAlpha) {
   return hasAlpha ? MODE_bgrA : MODE_BGRA;
 }
+#elif SK_PMCOLOR_BYTE_ORDER(A, R, G, B)
+inline WEBP_CSP_MODE outputMode(bool hasAlpha) {
+  return hasAlpha ? MODE_Argb : MODE_ARGB;
+}
+#else
+#error Component order not supported by libwebp
 #endif
 
 namespace {
