aea7fdbcc301fc0b10f6d638925714ef010b628d
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-03-06 17:12:48 +0100

    FreeBSD qtwebengine patches from ports collection

--- qtwebengine/src/3rdparty/chromium/base/files/file_util_posix.cc
+++ qtwebengine/src/3rdparty/chromium/base/files/file_util_posix.cc
@@ -407,7 +407,7 @@ bool CopyDirectoryExcl(const FilePath& from_path,
 #endif  // !defined(OS_NACL_NONSFI)
 
 bool CreateLocalNonBlockingPipe(int fds[2]) {
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_BSD)
   return pipe2(fds, O_CLOEXEC | O_NONBLOCK) == 0;
 #else
   int raw_fds[2];
