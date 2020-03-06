62df067674598dc1dc846ebddb7b34e2bf5b05ba
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-03-06 17:12:48 +0100

    FreeBSD qtwebengine patches from ports collection

--- qtwebengine/src/3rdparty/chromium/ipc/ipc_channel_mojo.cc
+++ qtwebengine/src/3rdparty/chromium/ipc/ipc_channel_mojo.cc
@@ -64,7 +64,7 @@ class MojoChannelFactory : public ChannelFactory {
 };
 
 base::ProcessId GetSelfPID() {
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_BSD)
   if (int global_pid = Channel::GetGlobalPid())
     return global_pid;
 #endif  // OS_LINUX
