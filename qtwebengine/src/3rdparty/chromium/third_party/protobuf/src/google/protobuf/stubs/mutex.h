0adeb22c443a87ec72f939c0419fdd80c5a6e869
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-04-23 19:41:32 +0200

    tmp_protobuf.diff

--- qtwebengine/src/3rdparty/chromium/third_party/protobuf/src/google/protobuf/stubs/mutex.h
+++ qtwebengine/src/3rdparty/chromium/third_party/protobuf/src/google/protobuf/stubs/mutex.h
@@ -48,14 +48,28 @@ namespace internal {
 class LIBPROTOBUF_EXPORT WrappedMutex {
  public:
   WrappedMutex() = default;
-  void Lock() { mu_.lock(); }
-  void Unlock() { mu_.unlock(); }
+  void Lock() {
+int dummy;
+fprintf(stderr, "*** %p: %s\n", &dummy, __PRETTY_FUNCTION__);
+#if !defined(__FreeBSD__) || 1
+    mu_.lock();
+#endif
+  }
+  void Unlock() {
+int dummy;
+fprintf(stderr, "*** %p: %s\n", &dummy, __PRETTY_FUNCTION__);
+#if !defined(__FreeBSD__) || 1
+    mu_.unlock();
+#endif
+  }
   // Crash if this Mutex is not held exclusively by this thread.
   // May fail to crash when it should; will never crash when it should not.
   void AssertHeld() const {}
 
+#if !defined(__FreeBSD__) || 1
  private:
   std::mutex mu_;
+#endif
 };
 
 using Mutex = WrappedMutex;
