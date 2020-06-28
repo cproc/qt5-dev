bbf48abde54be3d1f39be2599819ab5a770a3be4
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-06-28 19:15:27 +0200

    qtwebengine_freebsd_crossbuild.patch

--- qtwebengine/src/3rdparty/chromium/third_party/blink/renderer/platform/wtf/assertions.cc
+++ qtwebengine/src/3rdparty/chromium/third_party/blink/renderer/platform/wtf/assertions.cc
@@ -48,7 +48,7 @@
 #include <windows.h>
 #endif
 
-#if defined(OS_MACOSX) || ((defined(OS_LINUX) || defined(OS_BSD)) && !defined(__UCLIBC__))
+#if (defined(OS_MACOSX) || ((defined(OS_LINUX) || defined(OS_BSD)) && !defined(__UCLIBC__))) && 0
 #include <cxxabi.h>
 #include <dlfcn.h>
 #include <execinfo.h>
