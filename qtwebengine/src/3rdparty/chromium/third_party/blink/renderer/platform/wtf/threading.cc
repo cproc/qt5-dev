8a3e16c88040ebc31e9523d068d918e51151873c
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-06-28 19:15:27 +0200

    FreeBSD qtwebengine patches from ports collection

--- qtwebengine/src/3rdparty/chromium/third_party/blink/renderer/platform/wtf/threading.cc
+++ qtwebengine/src/3rdparty/chromium/third_party/blink/renderer/platform/wtf/threading.cc
@@ -17,10 +17,14 @@
 #error Blink does not support threading on your platform.
 #endif
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_BSD)
 #include <sys/syscall.h>
 #elif defined(OS_ANDROID)
 #include <sys/types.h>
+#endif
+
+#if defined(OS_BSD)
+#include <pthread_np.h>
 #endif
 
 namespace WTF {
@@ -38,6 +42,8 @@ ThreadIdentifier CurrentThreadSyscall() {
   return syscall(__NR_gettid);
 #elif defined(OS_ANDROID)
   return gettid();
+#elif defined(OS_BSD)
+  return pthread_getthreadid_np();
 #else
   return reinterpret_cast<uintptr_t>(pthread_self());
 #endif
--- qtwebengine/src/3rdparty/gn/build/gen.py
+++ qtwebengine/src/3rdparty/gn/build/gen.py
@@ -44,7 +44,7 @@ class Platform(object):
 
   @staticmethod
   def known_platforms():
-    return ['linux', 'darwin', 'msvc', 'aix', 'fuchsia']
+    return ['linux', 'darwin', 'msvc', 'aix', 'fuchsia', 'freebsd']
 
   def platform(self):
     return self._platform
@@ -66,6 +66,9 @@ class Platform(object):
 
   def is_aix(self):
     return self._platform == 'aix'
+
+  def is_freebsd(self):
+    return self._platform == 'freebsd'
 
   def is_posix(self):
     return self._platform in ['linux', 'freebsd', 'darwin', 'aix']
@@ -362,6 +365,11 @@ def WriteGNNinja(path, platform, host, options):
     elif platform.is_aix():
       cflags_cc.append('-maix64')
       ldflags.append('-maix64')
+    elif platform.is_freebsd():
+      cflags.extend(['-Wno-deprecated-register', '-Wno-parentheses-equality'])
+      ldflags.extend(['-pthread'])
+      libs.extend(['-lexecinfo', '-lkvm', '-lutil'])
+      include_dirs += ['/usr/local/include']
 
     if platform.is_posix():
       ldflags.append('-pthread')
