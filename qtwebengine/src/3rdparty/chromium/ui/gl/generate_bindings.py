eaae08f55fa665f75aafa088dcef25c3156115d1
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-06-28 19:15:27 +0200

    FreeBSD qtwebengine patches from ports collection

--- qtwebengine/src/3rdparty/chromium/ui/gl/generate_bindings.py
+++ qtwebengine/src/3rdparty/chromium/ui/gl/generate_bindings.py
@@ -2627,6 +2627,9 @@ GLX_FUNCTIONS = [
   'arguments':
       'Display* dpy, GLXDrawable drawable, int32_t* numerator, '
       'int32_t* denominator' },
+{ 'return_type': '__GLXextFuncPtr',
+  'names': ['glXGetProcAddressARB'],
+  'arguments': 'const GLubyte* procName', },
 { 'return_type': 'void',
   'names': ['glXGetSelectedEvent'],
   'arguments': 'Display* dpy, GLXDrawable drawable, unsigned long* mask', },
