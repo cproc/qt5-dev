881bc6844842b5049366130f23e79ebaf1ff060d
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-06-28 19:15:27 +0200

    FreeBSD qtwebengine patches from ports collection

--- qtwebengine/src/3rdparty/chromium/extensions/browser/browser_context_keyed_service_factories.cc
+++ qtwebengine/src/3rdparty/chromium/extensions/browser/browser_context_keyed_service_factories.cc
@@ -93,7 +93,7 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
 #if defined(OS_CHROMEOS)
   NetworkingConfigServiceFactory::GetInstance();
 #endif
-#if defined(OS_LINUX) || defined(OS_WIN) || defined(OS_MACOSX)
+#if defined(OS_LINUX) || defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_BSD)
   NetworkingPrivateEventRouterFactory::GetInstance();
 #endif
   PowerAPI::GetFactoryInstance();
