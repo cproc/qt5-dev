b13ce5317d341ac098b9ee9322b54e30f3a8ec36
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-06-28 19:15:27 +0200

    FreeBSD qtwebengine patches from ports collection

--- qtwebengine/src/3rdparty/chromium/content/public/common/content_switches.h
+++ qtwebengine/src/3rdparty/chromium/content/public/common/content_switches.h
@@ -276,7 +276,7 @@ CONTENT_EXPORT extern const char kEnableOSKOverscroll[];
 CONTENT_EXPORT extern const char kDisablePanelFitting[];
 #endif
 
-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
+#if (defined(OS_LINUX) && !defined(OS_CHROMEOS)) || defined(OS_FREEBSD)
 CONTENT_EXPORT extern const char kEnableSpeechDispatcher[];
 #endif
 
