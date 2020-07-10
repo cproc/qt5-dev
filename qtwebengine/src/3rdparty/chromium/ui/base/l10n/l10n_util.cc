8de17735b223677597709afaef2f5f94ad8b841c
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-07-10 17:13:51 +0200

    qtwebengine_genode.diff

--- qtwebengine/src/3rdparty/chromium/ui/base/l10n/l10n_util.cc
+++ qtwebengine/src/3rdparty/chromium/ui/base/l10n/l10n_util.cc
@@ -717,6 +717,7 @@ base::string16 GetStringFUTF16(int message_id,
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
   const base::string16& format_string = rb.GetLocalizedString(message_id);
 
+#if 0
 #ifndef NDEBUG
   // Make sure every replacement string is being used, so we don't just
   // silently fail to insert one. If |offsets| is non-NULL, then don't do this
@@ -741,6 +742,7 @@ base::string16 GetStringFUTF16(int message_id,
       }
     }
   }
+#endif
 #endif
 
   base::string16 formatted = base::ReplaceStringPlaceholders(
