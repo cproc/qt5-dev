206d0b9d3d2e3044bcb04b6c2445e47245203af9
Christian Prochaska <christian.prochaska@genode-labs.com>
2021-08-24 16:50:16 +0200

    FreeBSD qtwebengine patches from ports collection

--- qtwebengine/src/3rdparty/chromium/third_party/webrtc/rtc_base/string_utils.h
+++ qtwebengine/src/3rdparty/chromium/third_party/webrtc/rtc_base/string_utils.h
@@ -25,11 +25,11 @@
 #endif  // WEBRTC_WIN
 
 #if defined(WEBRTC_POSIX)
-#ifdef BSD
+#if defined(WEBRTC_BSD) || defined(BSD)
 #include <stdlib.h>
-#else  // BSD
+#else  // WEBRTC_BSD
 #include <alloca.h>
-#endif  // !BSD
+#endif  // !WEBRTC_BSD
 #include <strings.h>
 #endif  // WEBRTC_POSIX
 
