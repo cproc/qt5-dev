fe7ca3726588b3a66d66773c2451baeb4cd580fc
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-08-10 06:33:22 +0200

    qtwebengine_freebsd.patch

--- qtwebengine/src/3rdparty/chromium/extensions/common/image_util.cc
+++ qtwebengine/src/3rdparty/chromium/extensions/common/image_util.cc
@@ -18,7 +18,11 @@
 #include "base/strings/stringprintf.h"
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
+#if defined(OS_BSD)
+#include <re2/re2.h>
+#else
 #include "third_party/re2/src/re2/re2.h"
+#endif // defined(OS_BSD)
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkColor.h"
