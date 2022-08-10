66b19cb9016e5156cf252f8cff7c4c16fc337bf6
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-08-10 06:33:22 +0200

    qtwebengine_freebsd.patch

--- qtwebengine/src/3rdparty/gn/build/gen.py
+++ qtwebengine/src/3rdparty/gn/build/gen.py
@@ -69,6 +69,9 @@ class Platform(object):
   def is_aix(self):
     return self._platform == 'aix'
 
+  def is_freebsd(self):
+    return self._platform == 'freebsd'
+
   def is_posix(self):
     return self._platform in ['linux', 'freebsd', 'darwin', 'aix', 'openbsd']
 
@@ -373,6 +376,11 @@ def WriteGNNinja(path, platform, host, options):
     elif platform.is_aix():
       cflags_cc.append('-maix64')
       ldflags.append('-maix64')
+    elif platform.is_freebsd():
+      cflags.extend(['-Wno-deprecated-register', '-Wno-parentheses-equality'])
+      ldflags.extend(['-pthread'])
+      libs.extend(['-lexecinfo', '-lkvm', '-lutil'])
+      include_dirs += ['%%LOCALBASE%%/include']
 
     if platform.is_posix():
       ldflags.append('-pthread')
