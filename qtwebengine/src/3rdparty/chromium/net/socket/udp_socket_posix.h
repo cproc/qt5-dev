6320fe7d2550d26c695130afde4cd7056774dbee
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-08-10 06:33:22 +0200

    qtwebengine_freebsd.patch

--- qtwebengine/src/3rdparty/chromium/net/socket/udp_socket_posix.h
+++ qtwebengine/src/3rdparty/chromium/net/socket/udp_socket_posix.h
@@ -34,7 +34,7 @@
 
 #if defined(__ANDROID__) && defined(__aarch64__)
 #define HAVE_SENDMMSG 1
-#elif defined(OS_LINUX)
+#elif defined(OS_LINUX) || defined(OS_BSD)
 #define HAVE_SENDMMSG 1
 #else
 #define HAVE_SENDMMSG 0
