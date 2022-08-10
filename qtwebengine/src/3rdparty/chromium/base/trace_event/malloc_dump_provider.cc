2ce57804a7354f112c0c93ddf405ce011e8bc5dd
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-08-10 06:33:22 +0200

    qtwebengine_freebsd.patch

--- qtwebengine/src/3rdparty/chromium/base/trace_event/malloc_dump_provider.cc
+++ qtwebengine/src/3rdparty/chromium/base/trace_event/malloc_dump_provider.cc
@@ -17,6 +17,8 @@
 
 #if defined(OS_MACOSX)
 #include <malloc/malloc.h>
+#elif defined(OS_BSD)
+#include <stdlib.h>
 #else
 #include <malloc.h>
 #endif
@@ -132,6 +134,9 @@ bool MallocDumpProvider::OnMemoryDump(const MemoryDumpArgs& args,
   }
 #elif defined(OS_FUCHSIA)
 // TODO(fuchsia): Port, see https://crbug.com/706592.
+#elif defined(OS_BSD)
+  total_virtual_size = 0;
+  allocated_objects_size = 0;
 #else
   struct mallinfo info = mallinfo();
   // In case of Android's jemalloc |arena| is 0 and the outer pages size is
