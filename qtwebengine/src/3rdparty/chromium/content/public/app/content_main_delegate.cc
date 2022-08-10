f863d1c51856bc6d0652b39ce51af456576a00f6
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-08-10 06:33:22 +0200

    qtwebengine_freebsd.patch

--- qtwebengine/src/3rdparty/chromium/content/public/app/content_main_delegate.cc
+++ qtwebengine/src/3rdparty/chromium/content/public/app/content_main_delegate.cc
@@ -27,13 +27,13 @@ int ContentMainDelegate::RunProcess(
   return -1;
 }
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_BSD)
 
 void ContentMainDelegate::ZygoteStarting(
     std::vector<std::unique_ptr<service_manager::ZygoteForkDelegate>>*
         delegates) {}
 
-#endif  // defined(OS_LINUX)
+#endif  // defined(OS_LINUX) || defined(OS_BSD)
 
 int ContentMainDelegate::TerminateForFatalInitializationError() {
   CHECK(false);
