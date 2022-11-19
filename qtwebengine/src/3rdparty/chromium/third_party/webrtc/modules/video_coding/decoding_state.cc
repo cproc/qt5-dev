c130f1b1ede257629e06dd45e89334efccd00717
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-11-19 08:25:01 +0100

    https://github.com/genodelabs/genode/issues/4132

--- qtwebengine/src/3rdparty/chromium/third_party/webrtc/modules/video_coding/decoding_state.cc
+++ qtwebengine/src/3rdparty/chromium/third_party/webrtc/modules/video_coding/decoding_state.cc
@@ -8,6 +8,8 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
+#include <cstdint>
+
 #include "modules/video_coding/decoding_state.h"
 
 #include "common_video/h264/h264_common.h"
