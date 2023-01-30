31f7b10c8588ec4aabaac35194c6a369f629a378
Christian Prochaska <christian.prochaska@genode-labs.com>
2023-01-30 08:46:59 +0100

    qtwebengine_genode.patch

--- qtwebengine/src/3rdparty/chromium/base/files/file_util_posix.cc
+++ qtwebengine/src/3rdparty/chromium/base/files/file_util_posix.cc
@@ -411,7 +411,7 @@ bool CreatePipe(ScopedFD* read_fd, ScopedFD* write_fd, bool non_blocking) {
 }
 
 bool CreateLocalNonBlockingPipe(int fds[2]) {
-#if defined(OS_LINUX) || defined(OS_BSD)
+#if (defined(OS_LINUX) || defined(OS_BSD)) && 0
   return pipe2(fds, O_CLOEXEC | O_NONBLOCK) == 0;
 #else
   int raw_fds[2];
@@ -445,6 +445,7 @@ bool SetNonBlocking(int fd) {
 }
 
 bool SetCloseOnExec(int fd) {
+#if 0
 #if defined(OS_NACL_NONSFI)
   const int flags = 0;
 #else
@@ -456,6 +457,7 @@ bool SetCloseOnExec(int fd) {
 #endif  // defined(OS_NACL_NONSFI)
   if (HANDLE_EINTR(fcntl(fd, F_SETFD, flags | FD_CLOEXEC)) == -1)
     return false;
+#endif
   return true;
 }
 
@@ -836,7 +838,12 @@ int ReadFile(const FilePath& filename, char* data, int max_size) {
 
 int WriteFile(const FilePath& filename, const char* data, int size) {
   ScopedBlockingCall scoped_blocking_call(FROM_HERE, BlockingType::MAY_BLOCK);
+#if defined(OS_GENODE)
+  /* 'creat' is currently not implemented on Genode */
+  int fd = HANDLE_EINTR(open(filename.value().c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0666));
+#else
   int fd = HANDLE_EINTR(creat(filename.value().c_str(), 0666));
+#endif
   if (fd < 0)
     return -1;
 
@@ -881,6 +888,7 @@ bool AllocateFileRegion(File* file, int64_t offset, size_t size) {
     return false;
   }
 
+#if 0
   // Realize the extent of the file so that it can't fail (and crash) later
   // when trying to write to a memory page that can't be created. This can
   // fail if the disk is full and the file is sparse.
@@ -929,7 +937,7 @@ bool AllocateFileRegion(File* file, int64_t offset, size_t size) {
       return false;  // Can't write? Not viable.
     }
   }
-
+#endif
   return true;
 }
 
@@ -1076,6 +1084,9 @@ bool GetShmemTempDir(bool executable, FilePath* path) {
     *path = FilePath("/dev/shm");
     return true;
   }
+#elif defined(OS_FREEBSD)
+  *path = FilePath("/shm");
+  return true;
 #endif  // defined(OS_LINUX) || defined(OS_AIX)
   return GetTempDir(path);
 }
