bef7d954146f846d59a5aff2c5befba3259dc602
Christian Prochaska <christian.prochaska@genode-labs.com>
2024-08-19 09:47:49 +0200

    qtbase_genode.patch

--- qtbase/src/gui/painting/qbrush.cpp
+++ qtbase/src/gui/painting/qbrush.cpp
@@ -1422,7 +1422,12 @@ QGradient::~QGradient()
 }
 
 QT_END_NAMESPACE
-static void initGradientPresets() { Q_INIT_RESOURCE(qmake_webgradients); }
+static void initGradientPresets()
+{
+#ifndef Q_OS_GENODE
+	Q_INIT_RESOURCE(qmake_webgradients);
+#endif
+}
 Q_CONSTRUCTOR_FUNCTION(initGradientPresets);
 QT_BEGIN_NAMESPACE
 
